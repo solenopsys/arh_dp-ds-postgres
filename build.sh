e8d0ebbf904c312da96e7f533aa6c5ccc5e0f77e
AlexStorm <info@alexstorm.ru>
2023-04-03 14:41:03 +0530

    pg v 15

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ helm_build_push(){
   FN=${NAME}-${VER}.tgz
   # rm ${FN}
   helm package ./install --version ${VER}
-  curl --data-binary "@${FN}" http://helm.alexstorm.solenopsys.org/api/charts
+  curl --data-binary "@${FN}" http://helm.solenopsys.org/api/charts
 }
 
 NAME=solenopsys-ds-postgres
 ARCHS="linux/amd64,linux/arm64"
-VER=0.1.1
+VER=0.1.3
 
 helm_build_push
 
